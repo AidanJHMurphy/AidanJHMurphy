86f6cd42c6e6d2542f2703e05250a74cae52b278
Aidan Murphy <AidanJHMurphy@Gmail.com>
2023-10-22 18:10:07 -0500

    add configs for barbar

--- nvim/lua/core/plugin_config/barbar.lua
+++ nvim/lua/core/plugin_config/barbar.lua
@@ -6,3 +6,23 @@ local opts = {
 vim.api.nvim_set_keymap('n', '<leader>x', '<Cmd>BufferClose<CR>', opts)
 vim.api.nvim_set_keymap('n', '<TAB>', '<Cmd>BufferNext<CR>', opts)
 vim.api.nvim_set_keymap('n', '<S-TAB>', '<Cmd>BufferPrevious<CR>', opts)
+
+vim.g.barbar_auto_setup = false
+
+require'barbar'.setup {
+    auto_hide = 0,
+    
+    hide = {inactive = true},
+
+    icons = {
+        gitsigns = {
+            added = {enabled = true, icon = '+'},
+            changed = {enabled = true, icon = '~'},
+            deleted = {enabled = true, icon = '-'},
+        },
+    },
+
+    sidebar_filetypes = {
+        NvimTree = true,
+    },
+}
