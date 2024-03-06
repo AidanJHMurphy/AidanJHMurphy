4924840fdd148cbcf3515745e0bc77eb81604b93
Aidan Murphy <AidanJHMurphy@Gmail.com>
2024-03-06 12:12:48 -0800

    don't hid inactive tabs

--- nvim/lua/core/plugin_config/barbar.lua
+++ nvim/lua/core/plugin_config/barbar.lua
@@ -12,7 +12,7 @@ vim.g.barbar_auto_setup = false
 require'barbar'.setup {
     auto_hide = 0,
     
-    hide = {inactive = true},
+    hide = {inactive = false},
 
     icons = {
         gitsigns = {
