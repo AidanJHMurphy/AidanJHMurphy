28480c75e2518f4a5381fd23e0e90c6ab30a5c48
Aidan Murphy <AidanJHMurphy@Gmail.com>
2024-03-11 22:15:39 -0700

    Removing unused flags in nixos automatic updates

--- configuration.nix
+++ configuration.nix
@@ -230,12 +230,6 @@ in
   # Automatically update
   system.autoUpgrade = {
     enable = true;
-    # flake = inputs.self.outPath;
-    flags = [
-      "--update-input"
-      "nixpkgs"
-      "-L" # print build logs
-    ];
     dates = "02:00";
     randomizedDelaySec = "45min";
   };
