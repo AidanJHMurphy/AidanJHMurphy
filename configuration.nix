5fa799ff0ca41051eb4f697c4e112bd803e89cfb
Aidan Murphy <AidanJHMurphy@Gmail.com>
2024-06-05 21:49:03 -0500

    add ntfs support

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,7 @@ in {
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = ["ntfs"];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
