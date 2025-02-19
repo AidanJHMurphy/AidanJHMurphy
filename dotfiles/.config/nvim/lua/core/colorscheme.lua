a58b34b33c8b1cb008894bdfc220b0a489f6d291
Aidan Murphy <AidanJHMurphy@Gmail.com>
2025-02-19 14:04:05 -0800

    update vim colorsheme option name

--- dotfiles/.config/nvim/lua/core/colorscheme.lua
+++ dotfiles/.config/nvim/lua/core/colorscheme.lua
@@ -1,6 +1,6 @@
 local theme = "gruvbox"
 
-vim.o.termcolors = true
+vim.opt.termguicolors = true
 local ok, _ = pcall(vim.cmd, "colorscheme " .. theme)
 if not ok then
     vim.cmd 'colorscheme default'
