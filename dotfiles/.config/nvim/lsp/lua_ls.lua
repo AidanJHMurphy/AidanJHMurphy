14b5e5d1d0a94a680eeb5474c9bde98017817a1a
Aidan Murphy <AidanJHMurphy@Gmail.com>
2025-10-28 13:56:20 -0700

    add lsp file for lua_ls

---
+++ dotfiles/.config/nvim/lsp/lua_ls.lua
@@ -0,0 +1,16 @@
+return {
+    settings = {
+        Lua = {
+            diagnostics = {
+                -- recognize vim global
+                globals = { "vim" },
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}
